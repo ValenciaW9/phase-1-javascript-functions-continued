8ca8b132acc0e3f9dc64703a618820812bc388c8
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-02 15:13:10 -0400

    phase-1-javascript-functions-continued/
    phase-1-javascript-functions-continued/

--- phase-1-javascript-functions-continued-master/index.js
+++ phase-1-javascript-functions-continued-master/index.js
@@ -1,14 +1,5 @@
-// code your solution here
-const mondayWork = function(activity = 'go to the office') {
-  return `This Monday, I will ${activity}.`;
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
 }
-const wrapAdjective = function(character = '*') {
-  return function(adjective) {
-    return `You are ${character}${adjective}${character}!`;
-  }
-}console.log(saturdayFun("go hiking"));
+console.log(saturdayFun("go hiking"));
 // Output: "This Saturday, I want to go hiking!"
-
-
-const highlight = wrapAdjective('~');
-console.log(highlight('awesome')); // Output: "You are ~awesome~!"
