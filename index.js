945a77faedf1b38ed324a6bb0d9055331fd0e94e
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-02 14:59:56 -0400

    phase-1-javascript-functions-continued/
    phase-1-javascript-functions-continued/

--- index.js
+++ index.js
@@ -15,3 +15,6 @@ function razzle() {
   function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`;
   }
+
+  console.log(saturdayFun("go hiking"));
+// Output: "This Saturday, I want to go hiking!"
