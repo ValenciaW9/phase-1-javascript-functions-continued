c8e9c5f000126a3c0a18b7c9ce3fa7ed8e01ba08
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-02 15:17:55 -0400

    phase-1-javascript-functions-continued/
    phase-1-javascript-functions-continued/

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function saturdayFun(activity = "roller-skate") {
-  return `This Saturday, I want to ${activity}!`;
+const mondayWork = function(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
 }
-console.log(saturdayFun("go hiking"));
-// Output: "This Saturday, I want to go hiking!"
+const wrapAdjective = function(character = '*') {
+  return function(adjective) {
+    return `You are ${character}${adjective}${character}!`;
