537cf5e917df6bb5d10cc6bab0dbca0658e1146c
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-02 15:21:00 -0400

    phase-1-javascript-functions-continued/
    phase-1-javascript-functions-continued/

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const mondayWork = function(activity = 'go to the office') {
   return `This Monday, I will ${activity}.`;
 }
+
 const wrapAdjective = function(character = '*') {
   return function(adjective) {
     return `You are ${character}${adjective}${character}!`;
+  }
+}
+
+console.log(mondayWork()); // Output: "This Monday, I will go to the office."
+console.log(mondayWork('work from home')); // Output: "This Monday, I will work from home."
+
+const highlight = wrapAdjective('~');
+console.log(highlight('awesome')); // Output: "You are ~awesome~!"
